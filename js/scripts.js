868e903fe51808570c7369b79aef24e9655adc83
mai cook <mailiecook@yahoo.com>
2020-03-26 16:03:47 -0700

    add else if statements in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -4,39 +4,26 @@ $(document).ready(function() {
     var q1 = parseInt($("#q1").val());
     var q2 = parseInt($("#q2").val());
     var q3 = parseInt($("#q3").val());
-    var celebrity = q1+q2+q3+q4+q5+q6;
+    var celebrity = q1+q2+q3;
 
-    if (celebrity = 9) {
+    if (celebrity < 4 ) {
       $(".ponyo").show();
       $(".sosuke").hide();
       $(".lisa").hide();
      
     }
-    else if (celebrity >= 10 && score <= 15) {
+    else if (celebrity >= 3 && score <= 7) {
       $(".sosuke").show();
       $(".ponyo").hide();
       $(".lisa").hide();
       
     }
-    else if (celebrity >= 15 && score <= 18) {
+    else if (celebrity >= 7 && score <= 10) {
       $(".lisa").show();
       $(".ponyo").hide();
       $(".sosuke").hide();
       
    
     }
-  });
-
-  $(".btn-lang").click(function() {
-    var lang = $(this).text().toLowerCase().replace("#", "");
-    $(".info-box-wrap").css('display', 'flex');
-    $('.info-box').show();
-    $("#info-" + lang).show();
-  });
-
-  $(".info-box-close").click(function() {
-    $(".info-box-wrap").css('display', 'none');
-    $('.info-box').hide();
-    $(".info-text").hide();
   });
 });
