78d09839cb4d8f5a811c1d0172247bea5f0daef2
Christine Augustine <blacchoy@gmail.com>
2020-03-26 21:35:15 -0700

    add retake button & h3 css styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,29 +6,27 @@ $(document).ready(function() {
     var q3 = parseInt($("#q3").val());
     var celebrity = q1+q2+q3;
 
+
     if (celebrity < 4 ) {
       $("#ponyo").toggle();
       $("#sosuke").hide();
       $("#lisa").hide();
-      $("#quiz").hide();
-      
-     
+      $('#quiz').hide();
     }
     else if (celebrity >= 3 && celebrity <= 7) {
       $("#sosuke").toggle();
       $("#ponyo").hide();
       $("#lisa").hide();
-      $("#quiz").hide();
-      
+      $('#quiz').hide();
     }
     else if (celebrity >= 7 && celebrity <= 10) {
       $("#lisa").toggle();
       $("#ponyo").hide();
       $("#sosuke").hide();
-      $("#quiz").hide();    
+      $('#quiz').hide();
     }
   });
-  // $('.btn').click(function(){
-  //   location.reload();
-  // })
+  $('.btn1').click(function(){
+    location.reload();
+  })
 });
